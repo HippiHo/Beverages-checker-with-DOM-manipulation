264a7a317e1f15d7945968ae9c79cd1ecca9bd10
Elise Maschke <elise.maschke@outlook.de>
2019-06-24 23:54:45 +0200

    advanced version

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 class BeveragesChecker {
   constructor() {
     console.log("Hello from the constructor");
-    this.registerEvents();
+    const beverageList = document.querySelector("ul");
+
+    this.checkList();
+    this.addBeverage(beverageList);
+    this.deleteBeverage(beverageList);
   }
 
-  registerEvents() {
+  checkList() {
     const veganChecker = document.querySelector(".vegan_checker");
     const carnivoreChecker = document.querySelector(".carnivore_checker");
     const veganDrinks = document.querySelectorAll(".vegan");
@@ -15,7 +19,7 @@ class BeveragesChecker {
         case true:
           veganDrinks.forEach(element => {
             element.style.fontWeight = "bold";
-            element.style.color = "green";
+            element.style.color = "darkgreen";
             element.style.textAlign = "left";
           });
           break;
@@ -34,7 +38,7 @@ class BeveragesChecker {
         case true:
           carnivoreDrinks.forEach(element => {
             element.style.fontWeight = "bold";
-            element.style.color = "red";
+            element.style.color = "darkmagenta";
             element.style.textAlign = "left";
           });
           break;
@@ -45,6 +49,58 @@ class BeveragesChecker {
             element.style.textAlign = "";
           });
           break;
+      }
+    });
+  }
+
+  addBeverage(beverageList) {
+    const form = document.querySelector("form");
+    const input = form.querySelector("#new");
+
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+
+      const newItem = document.createElement("li");
+      const choice = this.getRadioVal(form);
+
+      newItem.classList.add(choice);
+
+      const text = input.value.trim();
+
+      newItem.innerHTML = ` ${text} <i class="fas fa-minus-square"></i> `;
+
+      beverageList.appendChild(newItem);
+
+      input.value = "";
+
+      this.checkList();
+    });
+  }
+
+  getRadioVal(form) {
+    let val = "";
+
+    // get list of radio buttons with specified name
+    const radios = form.elements.choice;
+
+    for (let i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        val = radios[i].value; 
+        break; // Break out of for loop!
+      }
+    }
+    return val;
+  }
+
+  deleteBeverage(beverageList) {
+    console.log("deleteBeverage");
+    beverageList.addEventListener("click", e => {
+      if (e.target.classList.contains("fa-minus-square")) {
+        const beverage = e.target.closest("li");
+
+        console.log("Removed", beverage.innerText);
+
+        beverageList.removeChild(beverage);
       }
     });
   }
